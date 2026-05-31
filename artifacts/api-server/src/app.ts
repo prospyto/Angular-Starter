1343788d64235ccc667d4370f3ec26b4bbc38222
Prospere AZONGLAHOUN <prospereazonglahoun@gmail.com>
2026-05-31 14:54:42 +0000

    fix: fix pino-http typing for bundler moduleResolution

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,23 @@
 import express, { type Express } from "express";
 import cors from "cors";
+import type { IncomingMessage, ServerResponse } from "http";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
-
 app.use(
-  pinoHttp({
+  (pinoHttp as unknown as (opts: object) => (req: IncomingMessage, res: ServerResponse, next: () => void) => void)({
     logger,
     serializers: {
-      req(req) {
+      req(req: IncomingMessage & { id?: unknown; url?: string; method?: string }) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: ServerResponse) {
         return {
           statusCode: res.statusCode,
         };
@@ -28,7 +28,5 @@ app.use(
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
-
 export default app;
